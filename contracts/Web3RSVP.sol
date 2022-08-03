e2ae760c85a73fff3fee0c8632c54e4f12b5fe4c
Iqrah Nadeem <iiqrah.dev@gmail.com>
2022-08-04 02:57:58 +0300

    Create Event struct with necessary data
    Comments have been put to justify each data being stored in the contract

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,2 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+contract Web3RSVP{
+
+    // be picky about the data you store on-chain as it is expensive!
+    // eDataCID to store event name, description etc
+    // only store data that is required for on-chain functionality 
+    struct Event{
+
+        // unique event identifier
+        bytes32 eID;
+        // unique event content-identifier
+        string eDataCID;
+        // address of person creating the event
+        address eCreator;
+        // timestamp of when event starts
+        uint256 eTimeStart;
+        // maximum capacity for event registrants
+        uint256 eCapacity;
+        // amount to be deposited by each registrant
+        uint256 eDepositAmount;
+        // array to store addresses of everyone that registers
+        address[] eRegistrants;
+        // array to store addresses of registrants that check-in and attend the event
+        address[] eAttendees;
+        // to check if deposited amlont is paid or not
+        bool isPaid;
+    }
+
+}
