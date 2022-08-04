c8d7a3ce2137a2a4ff58899e00ab17bd14d40505
Iqrah Nadeem <iiqrah.dev@gmail.com>
2022-08-04 03:51:57 +0300

    Create addRegistrant function

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -48,5 +48,32 @@ contract Web3RSVP{
         idToEventMapping[eID] = Event(eID, eDataCID, eCreator, eTimeStart, eCapacity, eDepositAmount, eRegistrants, eAttendees, isPaid);
     }
 
+    // Create function to add registrants by using eID to identify which event they want to get added to
+    // Make it payable as ETH is required to put as stake to register
+    function addRegistrant(bytes32 eID) external payable{
+
+        // use eID to check Event mapping and get that struct for this function
+        Event storage thisEvent = idToEventMapping[eID];
+
+        // Check if event has started yet?
+        require(block.timestamp < thisEvent.eTimeStart, 'Too late to register');
+
+        // Check if event capacity is reached yet?
+        require(thisEvent.eRegistrants.length < thisEvent.eCapacity, 'Max capacity reached');
+
+        // Check if registrant has enough ETH?
+        require(msg.value == thisEvent.eDepositAmount, 'Not enough ETH to proceed');
+
+        // Check if registrant already registered previously? 
+        for (uint8 i = 0; i < thisEvent.eRegistrants.length; i++){
+            require( thisEvent.eRegistrants[i] != msg.sender, 'You are already registered!');
+        }
+
+        // After all checks, push the registrant's address to the required array
+        thisEvent.eRegistrants.push(payable(msg.sender));
+
+        
+    }
+
 
 }
