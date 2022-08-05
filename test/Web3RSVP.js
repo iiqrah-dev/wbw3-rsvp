cdc8d93d7cc85025bf4fdf102d69e2776fb938e8
Iqrah Nadeem <iiqrah.dev@gmail.com>
2022-08-06 02:17:55 +0300

    Write createEvent() function test

--- test/Web3RSVP.js
+++ test/Web3RSVP.js
@@ -2,13 +2,31 @@ const hre = require("hardhat");
 
 const main = async () => {
   // use hardhat (hre) to deploy contract locally
-  const contractFactory = await hre.ethers.contractFactory("Web3RSVP");
-  const contract = await contractFactory.deploy();
-  contract.deployed();
-  console.log("Contract deployed to: ", contract.address);
+  const rsvpcontractFactory = await hre.ethers.contractFactory("Web3RSVP");
+  const rsvpcontract = await rsvpcontractFactory.deploy();
+  rsvpcontract.deployed();
+  console.log("Contract deployed to: ", rsvpcontract.address);
 
   // get addresses from hardhat to test contract functionality
   const [deployer, add1, add2] = hre.ethers.getSigners();
+
+  // Declare and assign values needed for createEvent() function call
+  let eDataCID = "bafybeibhwfzx6oo5rymsxmkdxpmkfwyvbjrrwcl7cekmbzlupmp5ypkyfi";
+  let eTimeStart = 1718926200;
+  let eCapacity = 25;
+  let eDepositAmount = hre.ethers.utils.parseEther("1");
+
+  // call contract function as an await transaction
+  let txn = await rsvpcontract.createEvent(
+    eDataCID,
+    eTimeStart,
+    eCapacity,
+    eDepositAmount
+  );
+  // log all the data from createEvent() function call inside wait
+  let wait = await txn.wait();
+  // print the contents that we get back from createEvent() function call
+  console.log("New event created: ", wait);
 };
 
 const runMain = async () => {
