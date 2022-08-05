56302a971fc1900ff8e1303c0a7a83966377b19b
Iqrah Nadeem <iiqrah.dev@gmail.com>
2022-08-06 02:55:46 +0300

    Comment previously checked data

--- test/Web3RSVP.js
+++ test/Web3RSVP.js
@@ -38,7 +38,7 @@ const main = async () => {
   // call addNewRegistrant() function as a contract transaction
   txn = await rsvpcontract.addNewRegistrant(eID, { value: eDepositAmount });
   wait = await txn.wait();
-  console.log(wait.events[0].args);
+  // console.log(wait.events[0].args);
 
   // // call addNewRegistrant with same address to check failure
   // txn = await rsvpcontract.addNewRegistrant(eID, { value: eDepositAmount });
@@ -50,14 +50,14 @@ const main = async () => {
     .connect(add1)
     .addNewRegistrant(eID, { value: eDepositAmount });
   wait = await txn.wait();
-  console.log(wait.events[0].args);
+  // console.log(wait.events[0].args);
 
   // call addNewRegistrant with adddress2
   txn = await rsvpcontract
     .connect(add2)
     .addNewRegistrant(eID, { value: eDepositAmount });
   wait = await txn.wait();
-  console.log(wait.events[0].args);
+  // console.log(wait.events[0].args);
 
   // call checkInAllRegistrants()
   txn = await rsvpcontract.checkInAllRegistrants(eID);
